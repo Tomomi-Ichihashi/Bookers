e569b3768982119a1139a331a3c4a81a096dc7b3
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-03-08 09:51:38 +0000

    [Add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'books' => 'books#index'
+  get 'show' => 'books#show'
+  get 'edit' => 'books#edit'
+  root :to => 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
