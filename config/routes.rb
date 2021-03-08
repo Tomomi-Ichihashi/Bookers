a09a20084c018ac4801504a1c8d73e2912bc01f7
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-03-08 11:34:25 +0000

    [Update]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'books' => 'books#index'
-  get 'show' => 'books#show'
-  get 'edit' => 'books#edit'
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit'
   root :to => 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
