d259c5f62530bde2531c0f104877adf7cc67a001
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-03-09 09:56:12 +0000

    [Add]update&destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,33 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
   
   def create
     @book = Book.new(book_params)
-    book.save
-    redirect_to show_path(book.id)
+    @book.save
+    redirect_to book_path(@book.id)
   end
   
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to index_path
   end
   
   
@@ -23,4 +38,5 @@ class BooksController < ApplicationController
   end
   
 end
+
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root :to => 'homes#top'
   get 'books' => 'books#index', as: 'index'
-  post 'bokks' => 'books#create'
-  patch 'books/:id' => 'books#update', as: 'update'
-  get 'books/:id' => 'books#show', as: 'show'
+  post 'books' => 'books#create'
   get 'books/:id/edit' => 'books#edit', as: 'edit'
-  delete 'books/:id' => 'books#destroy', as: 'destroy'
+  get 'books/:id' => 'books#show', as: 'book'
+  patch 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy'
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
