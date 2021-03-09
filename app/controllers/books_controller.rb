4b2800998c822f966bb18e3efe38564b0a513dc7
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-03-09 04:31:06 +0000

    [Add]index.html.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,24 @@ class BooksController < ApplicationController
     @books = Book.all
   end
   
+  def create
+    @book = Book.new(book_params)
+    book.save
+    redirect_to show_path(book.id)
+  end
+  
   def show
   end
 
   def edit
   end
+  
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
+
