2997b399b39a1de6d4d7aea33490d187f107bf86
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-03-09 03:38:03 +0000

    [Add]route&top.html.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit'
   root :to => 'homes#top'
+  get 'books' => 'books#index', as: 'index'
+  post 'bokks' => 'books#create'
+  patch 'books/:id' => 'books#update', as: 'update'
+  get 'books/:id' => 'books#show', as: 'show'
+  get 'books/:id/edit' => 'books#edit', as: 'edit'
+  delete 'books/:id' => 'books#destroy', as: 'destroy'
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
